1de7b2353645321bc82f170c9da68450317b184a
Neha Fathima <nehaputhur443@gmail.com>
2026-04-22 16:00:06 +0530

    feat: Implmented the Sales Invoice popup

--- aqrar_ext/hooks.py
+++ aqrar_ext/hooks.py
@@ -4,3 +4,23 @@ app_publisher = "Enfono"
 app_description = "Customizations and Extensions for Aqrar"
 app_email = "nah@enfono.com"
 app_license = "mit"
+
+app_include_js = [
+	"/assets/aqrar_ext/js/sales_invoice_pos_total_popup.js",
+]
+
+fixtures = [
+	{
+		"dt": "Custom Field",
+		"filters": [
+			[
+				"name",
+				"in",
+				[
+					# Sales Invoice
+					"Sales Invoice-custom_payment_mode",
+				],
+			]
+		]
+	},
+]
